e813f56c7fc06d6f093f7cfa40b28f6dd7427784
Yohana Alexandra Becerra Becerra <jobysalex@gmail.com>
2023-05-09 12:34:34 -0500

    Union de repositorios

--- src/data.js
+++ src/data.js
@@ -1,1 +1,10 @@
+import ghibli from './data/ghibli/ghibli.js';
 
+export const getFilmsData = () => {
+  return ghibli.films.map(film => {
+    return {
+      title: film.title,
+      poster: film.poster
+    }
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import { getFilmsData } from './data.js';
 
+let filmsData = getFilmsData();
+
+filmsData.forEach(data => {
+  const h2 = document.createElement('h2');
+  const img = document.createElement('img');
+  h2.textContent = data.title;
+  img.src = data.poster;
+  document.getElementById("root").appendChild(h2);
+  document.getElementById("root"). appendChild(img);
+});
 
 
 
