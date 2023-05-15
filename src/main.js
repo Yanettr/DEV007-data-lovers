f48ad959a9ff6ed61d68494b36dcc725d06b0c14
Christian R. Lara <christianramirezlara1@gmail.com>
2023-05-15 09:52:23 -0600

    Fix flexbox bugs

--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ import { getFilmsData } from './data.js';
 let filmsData = getFilmsData();
 
 filmsData.forEach(data => {
+  const div = document.createElement('article');
+  div.classList.add('card')
   const h2 = document.createElement('h2');
   const img = document.createElement('img');
   const p = document.createElement('p');
   h2.textContent = data.title;
   img.src = data.poster;
   p.textContent = data.description;
-  document.getElementById("root").appendChild(h2);
-  document.getElementById("root"). appendChild(img);
-  document.getElementById("root").appendChild(p);
+
+
+  div.appendChild(h2)
+  div.appendChild(img)
+  div.appendChild(p)
+
+
+  document.getElementById("root").appendChild(div);
 });
-
-
