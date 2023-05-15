67cb28937cc037514028775aa552a31f50a51202
Yohana Alexandra Becerra Becerra <jobysalex@gmail.com>
2023-05-15 17:41:38 -0500

    Funcional al Boton option Producer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { getFilmsData } from './data.js';
+import { getFilmsData, getFilmsByProducer } from './data.js';
 // declaración de variable que muestra el array de películas al cargar en DOM
 let filmsData = getFilmsData();
 
 filmsData.forEach(data => {
   const div = document.createElement('article');
-  div.classList.add('card')
+  div.classList.add('card');
   const h2 = document.createElement('h2');
   const img = document.createElement('img');
   const p = document.createElement('p');
@@ -19,4 +19,19 @@ filmsData.forEach(data => {
 
 
   document.getElementById("root").appendChild(div);
+});
+
+const selectProducer = document.getElementById('selectProducer');
+selectProducer.addEventListener('change', (event) => {
+  const selectedProducer = event.target.value;
+  const filteredFilms = getFilmsByProducer(selectedProducer);
+  document.getElementById("root").textContent = "";
+  filteredFilms.forEach(data => {
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    h2.textContent = data.title;
+    img.src = data.poster;
+    document.getElementById("root").appendChild(h2);
+    document.getElementById("root"). appendChild(img);
+  });
 });
