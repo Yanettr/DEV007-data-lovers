814d4925be1c6f6fa0d90639b1b8ec03ac76c505
Yohana Alexandra Becerra Becerra <jobysalex@gmail.com>
2023-05-19 12:34:25 -0500

    implementacion de la funcion ordenar alfabeticamente

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { getFilmsData, getFilmsByProducer, getFilmsByTitle } from './data.js';
+import { getFilmsData, getFilmsByProducer, getFilmsByTitle, getFilmsInOrder } from './data.js';
 // declaración de variable que muestra el array de películas al cargar en DOM
-let filmsData = getFilmsData();
+const filmsData = getFilmsData();
 
 filmsData.forEach(data => {
   const div = document.createElement('article');
@@ -22,27 +22,27 @@ selectProducer.addEventListener('change', (event) => {
   const filteredFilms = getFilmsByProducer(selectedProducer);
   document.getElementById("root").textContent = "";
   filteredFilms.forEach(data => {
-  const div = document.createElement('article');
-  div.classList.add('card');
-  const h2 = document.createElement('h2');
-  const img = document.createElement('img');
-  const ins = document.createElement('ins');
-  const strong = document.createElement('strong');
-  const em = document.createElement('em');
-  const p = document.createElement('p');
-  h2.textContent = data.title;
-  img.src = data.poster; 
-  ins.textContent = "Producer: " + data.producer;
-  strong.textContent = "Director: " + data.director;
-  em.textContent = "Release Date: " + data.release_date;
-  p.textContent = "Description: " + data.description;
-  div.appendChild(h2);
-  div.appendChild(img);
-  div.appendChild(ins);
-  div.appendChild(strong);
-  div.appendChild(em); 
-  div.appendChild(p);   
-  document.getElementById("root"). appendChild(div);
+    const div = document.createElement('article');
+    div.classList.add('card');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const ins = document.createElement('ins');
+    const strong = document.createElement('strong');
+    const em = document.createElement('em');
+    const p = document.createElement('p');
+    h2.textContent = data.title;
+    img.src = data.poster; 
+    ins.textContent = "Producer: " + data.producer;
+    strong.textContent = "Director: " + data.director;
+    em.textContent = "Release Date: " + data.release_date;
+    p.textContent = "Description: " + data.description;
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(ins);
+    div.appendChild(strong);
+    div.appendChild(em); 
+    div.appendChild(p);   
+    document.getElementById("root"). appendChild(div);
   });
 });
 
@@ -54,7 +54,7 @@ function handleSearch(event) {
   const searchTerm = document.getElementById('searchFilm').value.toLowerCase();
   // Filtrar las películas que coincidan con la búsqueda
   const filteredFilms = getFilmsByTitle.byNameFilms(filmsData, searchTerm);
-    // Llamar a la función para mostrar los resultados filtrados
+  // Llamar a la función para mostrar los resultados filtrados
   displayFilms(filteredFilms);
 }
 
@@ -77,3 +77,24 @@ function displayFilms(films) {
     rootElement.appendChild(div);
   });
 }
+
+const selectOrder = document.getElementById('selectOrder');
+selectOrder.addEventListener('change', (event) => {
+   const orderedTitles = event.target.value;
+   console.log(orderedTitles);
+   const orderedFilms = getFilmsInOrder(orderedTitles);
+   document.getElementById("root").textContent = "";
+
+   orderedFilms.forEach(film => {
+    const div = document.createElement('article');
+    div.classList.add('card');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    h2.textContent = film.title;
+    img.src = film.poster;
+    div.appendChild(h2);
+    div.appendChild(img);
+    document.getElementById("root").appendChild(div);
+   });
+});
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,13 @@
-import { example, anotherExample } from '../src/data.js';
+import { getFilmsData, anotherExample } from '../src/data.js';
 
 
 describe('example', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof getFilmsData).toBe('function');
   });
 
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    expect( getFilmsData()).toBe('example');
   });
 });
 
